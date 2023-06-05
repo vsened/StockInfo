b4d15b10f5e14bd1029b4e5644160658f76b9118
vsened <vasily.senederov@gmail.com>
2023-06-05 20:40:12 +0500

    adding getIntradayInfo and CompanyInfo api

--- app/src/main/java/com/vsened/stockinfo/data/remote/StockApi.kt
+++ app/src/main/java/com/vsened/stockinfo/data/remote/StockApi.kt
@@ -1,5 +1,6 @@
 package com.vsened.stockinfo.data.remote
 
+import com.vsened.stockinfo.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,6 +12,18 @@ interface StockApi {
     suspend fun getListings(
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): CompanyInfoDto
 
     companion object {
         const val API_KEY = "YNQVR0RG28VR0P4V"
